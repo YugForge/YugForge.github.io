e3ac2bd92ca7962857136675a2a13ba0509ff76e
Utkarsh Shukla <utk.shukla@gmail.com>
2023-10-05 10:40:28 +0000

    updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'YugForge',
-  tagline: 'YugForge Editor',
+  tagline: 'Create your own Metaverse, Games and 3D Applications',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
